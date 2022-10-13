6818ececbd1fee73ceedc1a1bd991a379ce8aea9
linus.weigand <linus.weigand@physik.uni-muenchen.de>
2022-10-13 12:14:36 +0200

    Update tests

--- test/runtest.jl
+++ test/runtest.jl
@@ -2,6 +2,7 @@ using CudaRandomNumbers
 using CUDA
 using Distributions
 using Statistics
+using StatsBase
 using BenchmarkTools
 using Test
 
@@ -16,13 +17,21 @@ using Test
             @testset "Parameter regimes" begin
                 @testset "Uniform input array" begin
                     function test_mean_variance(A, λ, N)
-                        @test mean(A) ≈ var(A) rtol=sqrt(1/N) atol=1.0f-7
-                        @test mean(A) ≈ λ rtol=sqrt(1/N) atol=1.0f-7
+                        @test mean(A) ≈ var(A) rtol=sqrt(10/N) atol=1.0f-7
+                        @test mean(A) ≈ λ rtol=sqrt(10/N) atol=1.0f-7
+                    end
+
+                    function test_higher_cumulants(A, λ, N, n_cumulants)
+                        for i in 3:n_cumulants
+                            @test cumulant(A, i) ≈ λ rtol=sqrt(10/N) atol=1.0f-7
+                        end
                     end
                     @testset "λ = $λ, N = $N" for λ in [1.0f0, 5.0f0, 10.0f0, 15.0f0, 30.0f0], N in [2^7, 2^10, 2^13, 2^16, 2^19, 2^22]
                         A = CUDA.ones(FloatType, N)
                         CudaRandomNumbers.poisson_rho_dep!(rng, A, λ)
-                        test_mean_variance(A, λ, N)
+                        A_copy = Array(A)
+                        test_mean_variance(A_copy, λ, N)
+                        test_higher_cumulants(A_copy, λ, N, 10)
                     end
                 end
 
@@ -30,7 +39,21 @@ using Test
         
         end
         @testset "Gamma" begin
-        
+            @testset "Parameter regimes" begin
+                @testset "Uniform input array" begin
+                    function test_mean_variance(A, k, N)
+                        @test mean(A) ≈ k rtol=sqrt(10/N) atol=1.0f-7
+                        @test var(A) ≈ k rtol=sqrt(10/N) atol=1.0f-7
+                    end
+
+                    @testset "k = $k, N = $N" for k in [1.0f0, 5.0f0, 10.0f0, 15.0f0, 30.0f0], N in [2^7, 2^10, 2^13, 2^16, 2^19, 2^22]
+                        A = k*CUDA.ones(FloatType, N)
+                        CudaRandomNumbers.gamma_rho_dep!(rng, A)
+                        test_mean_variance(A, k, N)
+                    end
+                end
+
+            end
         end
     end
 end
