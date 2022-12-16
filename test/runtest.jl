220aa26c4e656ea0a98ecd042eb83a67e92e0f8b
linus.weigand <linus.weigand@physik.uni-muenchen.de>
2022-12-16 15:43:48 +0100

    A few more tests

--- test/runtest.jl
+++ test/runtest.jl
@@ -5,11 +5,12 @@ using Statistics
 using StatsBase
 using BenchmarkTools
 using Test
+using Random
 
 
 @testset "CudaRandomNumbers.jl" begin
     rng = CUDA.RNG()
-    @testset "FloatType = $FloatType" for FloatType in [Float32, Float64]
+    @testset "FloatType = $FloatType" for FloatType in [Float32]
         @testset "Poisson" begin
             @testset "Distributions" begin
             
@@ -17,21 +18,33 @@ using Test
             @testset "Parameter regimes" begin
                 @testset "Uniform input array" begin
                     function test_mean_variance(A, λ, N)
-                        @test mean(A) ≈ var(A) rtol=sqrt(10/N) atol=1.0f-7
-                        @test mean(A) ≈ λ rtol=sqrt(10/N) atol=1.0f-7
+                        @test mean(A) ≈ var(A) rtol=sqrt(100/N) atol=1.0f-7
+                        @test mean(A) ≈ λ rtol=sqrt(100/N) atol=1.0f-7
                     end
 
                     function test_higher_cumulants(A, λ, N, n_cumulants)
                         for i in 3:n_cumulants
-                            @test cumulant(A, i) ≈ λ rtol=sqrt(10/N) atol=1.0f-7
+                            @test cumulant(A, i) ≈ λ rtol=sqrt(100/N) atol=1.0f-7
                         end
                     end
-                    @testset "λ = $λ, N = $N" for λ in [1.0f0, 5.0f0, 10.0f0, 15.0f0, 30.0f0], N in [2^7, 2^10, 2^13, 2^16, 2^19, 2^22]
+                    @testset "GPU: λ = $λ, N = $N" for λ in [1.0f0, 5.0f0, 30.0f0], N in [2^10, 2^16, 2^19, 2^22]
                         A = CUDA.ones(FloatType, N)
                         CudaRandomNumbers.poisson_rho_dep!(rng, A, λ)
                         A_copy = Array(A)
                         test_mean_variance(A_copy, λ, N)
                         test_higher_cumulants(A_copy, λ, N, 10)
+                    end
+                    @testset "CPU: λ = $λ, N = $N" for λ in [1.0f0, 5.0f0, 30.0f0], N in [2^10, 2^16, 2^19, 2^22]
+                        #A = ones(FloatType, N)
+                        #CudaRandomNumbers.poisson_rho_dep!(A, λ)
+                        #A_copy = Array(A)
+                        #test_mean_variance(A_copy, λ, N)
+                        #test_higher_cumulants(A_copy, λ, N, 10)
+
+                        B = ones(FloatType, N)
+                        Random.rand!(Distributions.Poisson(λ),B)
+                        test_mean_variance(B, λ, N)
+                        test_higher_cumulants(B, λ, N, 10)
                     end
                 end
 
@@ -42,17 +55,22 @@ using Test
             @testset "Parameter regimes" begin
                 @testset "Uniform input array" begin
                     function test_mean_variance(A, k, N)
-                        @test mean(A) ≈ k rtol=sqrt(10/N) atol=1.0f-7
-                        @test var(A) ≈ k rtol=sqrt(10/N) atol=1.0f-7
+                        @test mean(A) ≈ k rtol=sqrt(100/N) atol=1.0f-7
+                        @test var(A) ≈ k rtol=sqrt(100/N) atol=1.0f-7
                     end
 
-                    @testset "k = $k, N = $N" for k in [1.0f0, 5.0f0, 10.0f0, 15.0f0, 30.0f0], N in [2^7, 2^10, 2^13, 2^16, 2^19, 2^22]
+                    @testset "GPU: k = $k, N = $N" for k in [1.0f0, 5.0f0, 10.0f0, 15.0f0, 30.0f0], N in [2^7, 2^10, 2^13, 2^16, 2^19, 2^22]
                         A = k*CUDA.ones(FloatType, N)
                         CudaRandomNumbers.gamma_rho_dep!(rng, A)
                         test_mean_variance(A, k, N)
                     end
-                end
 
+                    @testset "CPU: k = $k, N = $N" for k in [1.0f0, 5.0f0, 10.0f0, 15.0f0, 30.0f0], N in [2^7, 2^10, 2^13, 2^16, 2^19, 2^22]
+                        A = k*ones(FloatType, N)
+                        CudaRandomNumbers.gamma_rho_dep!(A)
+                        test_mean_variance(A, k, N)
+                    end
+                end
             end
         end
     end
