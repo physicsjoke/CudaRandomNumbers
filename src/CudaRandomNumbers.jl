afbe11e78b2c6552af38355a3066c629b463b0b1
linus.weigand <linus.weigand@physik.uni-muenchen.de>
2022-12-19 16:23:50 +0100

    Reorder stuff

--- src/CudaRandomNumbers.jl
+++ src/CudaRandomNumbers.jl
@@ -1,12 +1,13 @@
 module CudaRandomNumbers
 
+export gamma_rho_dep!, poisson_rho_dep!
 
 const FloatType=Float32
 
 include("Poisson.jl")
 include("Gamma.jl")
 
-export gamma_rho_dep!, poisson_rho_dep!
+
 
 
 end # module
