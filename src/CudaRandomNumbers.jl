7c2b00308a09fab1925359d3586521f26586702d
linus.weigand <linus.weigand@physik.uni-muenchen.de>
2023-01-24 14:47:24 +0100

    Remove broken ChangePrecision package

--- src/CudaRandomNumbers.jl
+++ src/CudaRandomNumbers.jl
@@ -3,14 +3,10 @@ module CudaRandomNumbers
 export rand_gamma, rand_poisson
 
 using StatsFuns
-using ChangePrecision
 const FloatType=Float32
 
-@changeprecision FloatType begin
     
 include("Poisson.jl")
 include("Gamma.jl")
 
-
-end # changeprecision FloatType
 end # module
